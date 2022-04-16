c83a387c7929cab035a7fd0266b83038044ce706
jinwoo <inx0930@gmail.com>
2022-04-16 10:56:11 +0900

    first commit

---
+++ src/main/java/com/example/dateplanning/DateplanningApplication.java
@@ -0,0 +1,13 @@
+package com.example.dateplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateplanningApplication.class, args);
+    }
+
+}
